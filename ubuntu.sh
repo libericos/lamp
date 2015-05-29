218ce1c1496e06315df28392672bbd476e3e53f3
root <root@vps173589.ovh.net>
2015-05-29 05:19:24 -0400

    sudo removed

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,13 +3,13 @@
 apt-get update
 apt-get install apache2
 
-sudo apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
+apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
 
-sudo mysql_install_db
+mysql_install_db
 
-sudo /usr/bin/mysql_secure_installation
+/usr/bin/mysql_secure_installation
 
-sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt
+apt-get install php5 libapache2-mod-php5 php5-mcrypt
 
 
 
